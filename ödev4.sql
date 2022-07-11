49f4c8af9cec0b094307145984fd0ae72e7ceb4d
“[Dogukan]” <[dogkantastan29@gmail.com]>
2022-07-11 22:09:49 +0300

    Ödev 4 Eklendi

---
+++ ödev4.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT replacement_cost FROM film;
+SELECT COUNT (DISTINCT replacement_cost) FROM film;
+SELECT COUNT(*) FROM film WHERE title LIKE 'T%' AND rating='G';
+SELECT COUNT(*) city FROM city WHERE city ILIKE '%R';
