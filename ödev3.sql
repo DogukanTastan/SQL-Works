275177130f6794482328c2c0360eabb443f8b139
“[Dogukan]” <[dogkantastan29@gmail.com]>
2022-07-11 21:49:38 +0300

    Ödev 3 Eklendi

---
+++ ödev3.sql
@@ -0,0 +1,4 @@
+SELECT country FROM country WHERE country LIKE 'A%a';
+SELECT country FROM country WHERE country LIKE '%n' AND country LIKE '%______%';
+SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';
+SELECT * FROM film WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;
