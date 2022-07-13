c21c0be89a16e7421fd15e3fb5c66748e0cbbaad
“[Dogukan]” <[dogkantastan29@gmail.com]>
2022-07-13 23:35:26 +0300

    Ödev 9 Eklendi

--- ödev_09.sql
+++ ödev_09.sql
@@ -5,13 +5,11 @@ FROM city
 INNER JOIN country
 ON city.country_id = country.country_id;
 
-
 --customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
 
 SELECT payment_id,first_name,last_name FROM customer
 INNER JOIN payment
 ON customer.customer_id=payment.customer_id;
-
 
 --customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
 
