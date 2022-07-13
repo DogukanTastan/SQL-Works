aba000125bba0c052e6a7eb44dfbdba694f96c82
“[Dogukan]” <[dogkantastan29@gmail.com]>
2022-07-13 23:28:24 +0300

    Ödev 1 Eklendi

--- ödev_01.sql
+++ ödev_01.sql
@@ -8,15 +8,9 @@ SELECT * FROM film WHERE (length>60 AND length<75);
 SELECT * FROM film WHERE ((rental_rate=0.99 AND replacement_cost=12.99)OR(replacement_cost= 28.99));
 
 --customer tablosunda bulunan first_name sütunundaki deðeri 'Mary' olan müþterinin last_name sütunundaki deðeri nedir?
-
 SELECT * FROM customer WHERE first_name='Mary';
 
 --film tablosundaki uzunluðu(length) 50 ten büyük OLMAYIP ayný zamanda rental_rate deðeri 2.99 veya 4.99 OLMAYAN verileri sýralayýnýz.
 SELECT * FROM film WHERE (length<50 AND rental_rate=2.99 OR (NOT rental_rate=4.99));
-
-
-
-
-
 
 
