50e951f0c50b60a0d474a799a80ced7b131d82e9
“[Dogukan]” <[dogkantastan29@gmail.com]>
2022-07-13 23:33:32 +0300

    Ödev 5 Eklendi

--- ödev_05.sql
+++ ödev_05.sql
@@ -3,15 +3,15 @@ SELECT * from film
 WHERE title LIKE '%n'
 ORDER BY length DESC
 LIMIT 5;
---film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kýsa 
---(length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sýralayýnýz.
+
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kýsa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sýralayýnýz.
 SELECT * FROM film
 WHERE title LIKE '%n'
 ORDER BY length
 OFFSET 5
 LIMIT 5;
---customer tablosunda bulunan last_name sütununa göre azalan yapýlan sýralamada 
---store_id 1 olmak koþuluyla ilk 4 veriyi sýralayýnýz.
+
+--customer tablosunda bulunan last_name sütununa göre azalan yapýlan sýralamada store_id 1 olmak koþuluyla ilk 4 veriyi sýralayýnýz.
 SELECT * FROM customer
 WHERE store_id IN(1)
 ORDER BY last_name DESC
