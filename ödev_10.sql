c11fc13f3746345153d30e05c1e490bff968d6a0
“[Dogukan]” <[dogkantastan29@gmail.com]>
2022-07-13 23:27:26 +0300

    ödev 10 Eklendi

--- ödev_10.sql
+++ ödev_10.sql
@@ -4,13 +4,11 @@ SELECT city.city,country.country FROM city
 LEFT JOIN country
 on city.city_id=country.country_id;
 
-
 --customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
 
 SELECT customer.first_name,customer.last_name, payment.payment_id FROM customer
 RIGHT JOIN payment
 ON customer.customer_id=payment.payment_id;
-
 
 --customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
 
